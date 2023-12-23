5ba55a1364aeaeeeccc645785e730db4fcb5015c
ikbarfp <ikbar@finansia.com>
2023-12-24 01:43:05 +0700

    chore: remove unnecessary request

--- internal/user/request.go
+++ internal/user/request.go
@@ -1,12 +1,9 @@
 package user
 
-import "time"
-
 // RequestRegister . . .
 type RequestRegister struct {
-	FullName        string    `json:"full_name"`
-	DateOfBirth     time.Time `json:"date_of_birth"`
-	MobilePhone     string    `json:"mobile_phone"`
-	Pin             string    `json:"pin"`
-	ConfirmationPin string    `json:"confirmation_pin"`
+	FullName        string `json:"full_name"`
+	MobilePhone     string `json:"mobile_phone"`
+	Pin             string `json:"pin"`
+	ConfirmationPin string `json:"confirmation_pin"`
 }
