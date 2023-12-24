6b39a00f5780d65fd2c885b7986e2992e15a38d7
ikbarfp <ikbar@finansia.com>
2023-12-24 13:32:13 +0700

    chore: update code comments on config option

--- pkg/config/option.go
+++ pkg/config/option.go
@@ -2,21 +2,21 @@ package config
 
 type OptionFunc func(*Config)
 
-// WithExtension . . .
+// WithExtension option function for stating config file extension
 func WithExtension(ext string) OptionFunc {
 	return func(cfg *Config) {
 		cfg.extension = ext
 	}
 }
 
-// WithFilename . . .
+// WithFilename option function for stating config file name
 func WithFilename(name string) OptionFunc {
 	return func(cfg *Config) {
 		cfg.name = name
 	}
 }
 
-// WithPath . . .
+// WithPath option function for stating config file path
 func WithPath(path string) OptionFunc {
 	return func(cfg *Config) {
 		cfg.path = path
